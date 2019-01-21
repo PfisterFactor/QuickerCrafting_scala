40732e63bdff75f83e743876fa05af1e33cb74c8
ej <EJPfister55@gmail.com>
2019-01-21 01:04:37 -0600

    Removed scrollbar, implemented pages

--- src/main/scala/pfister/quickercrafting/client/gui/GuiQuickerCrafting.scala
+++ src/main/scala/pfister/quickercrafting/client/gui/GuiQuickerCrafting.scala
@@ -1,17 +1,20 @@
 package pfister.quickercrafting.client.gui
 
+import java.util.Comparator
+
 import net.minecraft.client.gui.inventory.GuiContainer
+import net.minecraft.client.gui.{GuiButton, GuiButtonImage}
 import net.minecraft.client.renderer.{GlStateManager, RenderHelper}
 import net.minecraft.entity.player.InventoryPlayer
 import net.minecraft.item.crafting.IRecipe
 import net.minecraft.util.ResourceLocation
-import net.minecraft.util.math.MathHelper
-import org.lwjgl.input.Mouse
 import pfister.quickercrafting.QuickerCrafting
 import pfister.quickercrafting.client.RecipeCalculator
 import pfister.quickercrafting.common.gui.{ContainerQuickerCrafting, GuiButtonQuickRecipe}
 
 import scala.collection.JavaConversions._
+import scala.collection.mutable
+import scala.util.Try
 
 
 private[gui] object GuiQuickCrafting {
@@ -19,39 +22,48 @@ private[gui] object GuiQuickCrafting {
 }
 class GuiQuickerCrafting(playerInv: InventoryPlayer) extends GuiContainer(new ContainerQuickerCrafting(playerInv)) {
 
-  // Create a new array of recipe items the size of the amount of crafting buttons there are
-  private val displayedRecipeItems: Array[Option[IRecipe]] = new Array(32)
-
-  private var scrollbar: GuiScrollbar = _
+  private val cachedRecipes: mutable.ListBuffer[IRecipe] = mutable.ListBuffer()
 
   private var recipeCalculator: RecipeCalculator = new RecipeCalculator(playerInv)
 
+  private var recipeIterator: Iterator[IRecipe] = recipeCalculator.getRecipeIterator()
+
+  var currentPage: Int = 1
+  var maxPage: Int = 1
+
   // Set size of window
-  this.xSize = 196
+  this.xSize = 208
   this.ySize = 175
   //
 
   override def initGui(): Unit = {
     super.initGui()
-    // Create scrollbar
-    if (scrollbar != null)
-      scrollbar = scrollbar.copy(Left = guiLeft + 176, Right = guiLeft + 187, Top = guiTop + 8, Bottom = guiTop + 85)
-    else
-      scrollbar = GuiScrollbar(Left = guiLeft + 176, Right = guiLeft + 187, Top = guiTop + 8, Bottom = guiTop + 85, currentScroll = 0D)
-
     // Populate the recipe list with buttons
     for (y <- 0 until 4; x <- 0 until 8)
       buttonList.add(new GuiButtonQuickRecipe(y * 8 + x, this.guiLeft + 7 + x * 20, this.guiTop + 7 + y * 20, 20, 20, 236, 15, 20, GuiQuickCrafting.TEXTURE))
 
-    // Set random items to display
-    val recipes = recipeCalculator.getRecipeIterator()
-    for (i <- 0 until 32)
-      displayedRecipeItems(i) = if (recipes.hasNext) {
-        Some(recipes.next())
-      }
-      else None
+    buttonList.add(new GuiButtonImage(33, this.guiLeft + 183, this.guiTop + 7, 11, 7, 234, 0, 7, GuiQuickCrafting.TEXTURE))
+    buttonList.add(new GuiButtonImage(34, this.guiLeft + 183, this.guiTop + 27, 11, 7, 245, 0, 7, GuiQuickCrafting.TEXTURE))
+
+    recipeIterator.foreach(r => cachedRecipes.append(r))
+    cachedRecipes.sort(new Comparator[IRecipe] {
+      override def compare(o1: IRecipe, o2: IRecipe): Int =
+        o1.getRecipeOutput.getItem.getCreativeTab.getTabIndex - o2.getRecipeOutput.getItem.getCreativeTab.getTabIndex
+    })
+
+    maxPage = cachedRecipes.size / 32
+    if (cachedRecipes.size % 32 != 0) maxPage += 1
 
   }
+
+  override def actionPerformed(button: GuiButton): Unit = {
+    currentPage = button.id match {
+      case 33 => Math.max(currentPage - 1, 1)
+      case 34 => Math.min(currentPage + 1, maxPage)
+      case _ => currentPage
+    }
+  }
+
 
   // Draws the background to the GUI
   override def drawGuiContainerBackgroundLayer(partialTicks: Float, mouseX: Int, mouseY: Int): Unit = {
@@ -60,76 +72,55 @@ class GuiQuickerCrafting(playerInv: InventoryPlayer) extends GuiContainer(new Co
     this.mc.getTextureManager.bindTexture(GuiQuickCrafting.TEXTURE)
     this.drawTexturedModalRect(this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize)
 
-    // Draw the scrollbar
-    val scrollTextureOffset = if (scrollbar.isScrollNeeded()) 0 else 12
-    this.drawTexturedModalRect(scrollbar.Left, (scrollbar.Top + (scrollbar.Bottom - scrollbar.Top - 14) * scrollbar.CurrentScroll).toInt, 232 + scrollTextureOffset, 0, 12, 15)
   }
 
   // Draws the buttons and stuff on top of the background
   override def drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float): Unit = {
-    scrollbar.updateScrollbar(Mouse.isButtonDown(0), mouseX, mouseY)
-
     drawDefaultBackground()
     super.drawScreen(mouseX, mouseY, partialTicks)
     drawRecipeItems(partialTicks)
+    renderHoveredToolTip(mouseX, mouseY)
 
 
   }
 
+  override def drawGuiContainerForegroundLayer(mouseX: Int, mouseY: Int): Unit = {
+    val string = s"$currentPage/$maxPage"
+    this.fontRenderer.drawString(string, 176 - (string.length - 4) * 3, 17, 0)
+  }
 
   // Draws all the recipe items over the crafting buttons
   def drawRecipeItems(partialTicks: Float): Unit = {
-    buttonList.foreach(button => {
-      GlStateManager.pushMatrix()
-      GlStateManager.translate(2, 2, 0)
-      RenderHelper.disableStandardItemLighting()
-      RenderHelper.enableGUIStandardItemLighting()
+    buttonList.take(32).foreach(button => {
+      val recipe = Try(cachedRecipes((currentPage - 1) * 32 + button.id)).toOption
 
-      val recipe = displayedRecipeItems(button.id)
       if (recipe.isDefined) {
+        GlStateManager.pushMatrix()
+        GlStateManager.translate(2, 2, 0)
+        RenderHelper.disableStandardItemLighting()
+        RenderHelper.enableGUIStandardItemLighting()
         val itemstack = recipe.get.getRecipeOutput
         drawItemStack(itemstack, button.x, button.y, "" + itemstack.getCount)
+        button.enabled = true
+        GlStateManager.popMatrix()
       }
       else
         button.enabled = false
-
-
-      GlStateManager.popMatrix()
     })
 
   }
 
-
-}
-
-private case class GuiScrollbar(Left: Int, Right: Int, Top: Int, Bottom: Int, private var currentScroll: Double) {
-  private var mouseWasDown: Boolean = false
-
-  private var _isScrolling: Boolean = false
-
-  def isScrolling: Boolean = _isScrolling
-
-  private def mouseIsOver(x: Int, y: Int): Boolean = x > Left && x < Right && y > Top && y < Bottom
-
-  //noinspection AccessorLikeMethodIsEmptyParen
-  def isScrollNeeded(): Boolean = true
-
-  def CurrentScroll: Double = currentScroll
-
-  def updateScrollbar(mouseState: Boolean, mouseX: Int, mouseY: Int): Unit = {
-    if (!mouseWasDown && mouseState && mouseIsOver(mouseX, mouseY)) {
-      _isScrolling = isScrollNeeded()
-    }
-    if (!mouseState)
-      _isScrolling = false
-
-    mouseWasDown = mouseState
-    if (isScrolling) {
-      currentScroll = (mouseY - Top - 7.5D) / (Bottom - Top - 15.0D)
-      currentScroll = MathHelper.clamp(currentScroll, 0.0, 1.0)
+  override def renderHoveredToolTip(mouseX: Int, mouseY: Int) = {
+    super.renderHoveredToolTip(mouseX, mouseY)
+    val tooltipButton = buttonList.take(32).find(b => b.enabled && b.isMouseOver)
+    if (tooltipButton.isDefined) {
+      val recipe = Try(cachedRecipes.get((currentPage - 1) * 32 + tooltipButton.get.id)).toOption
+      if (recipe.isDefined)
+        renderToolTip(recipe.get.getRecipeOutput, mouseX, mouseY)
     }
 
   }
 
 
 }
+
