e6892db91b2212ac2ddd1d37fbca8e56c8620be3
ej <EJPfister55@gmail.com>
2019-08-09 14:06:20 -0500

    QoL stacking changes to craft result slots

--- src/main/scala/pfister/quickercrafting/common/util/CraftHandler.scala
+++ src/main/scala/pfister/quickercrafting/common/util/CraftHandler.scala
@@ -3,11 +3,15 @@ package pfister.quickercrafting.common.util
 import net.minecraft.item.crafting.IRecipe
 import pfister.quickercrafting.QuickerCrafting
 import pfister.quickercrafting.common.gui.ContainerQuickerCrafting
+import pfister.quickercrafting.common.util.ExtensionClasses.ExtInventoryBasic
 
+// Handles the functionality for crafting something
 object CraftHandler {
+  // Attempts to craft a recipe given the items within the container
   def tryCraftRecipe(container: ContainerQuickerCrafting, recipe: IRecipe): Boolean = {
     val recipeCalculator = new RecipeCalculator(container)
     val isServer = !container.PlayerInv.player.world.isRemote
+    // Get a map of items used and how much are used
     val itemsToRemove = recipeCalculator.doCraft(recipe)
 
     if (itemsToRemove.isEmpty) {
@@ -17,6 +21,7 @@ object CraftHandler {
     }
 
     if (!container.canFitStackInCraftResult(recipe.getRecipeOutput)) {
+      // Detect to see if after crafting there will be an open slot in the craft result slots, if there is we can put the item there
       val willCraftResultItemBeConsumed = itemsToRemove.get.exists(pair => {
         container.isCraftResultIndex(pair._1) && container.getSlot(pair._1).getStack.getCount <= pair._2
       })
@@ -26,13 +31,17 @@ object CraftHandler {
         return false
       }
     }
-
+    // Remove all items used during crafting
     itemsToRemove.get.foreach(pair => {
       val slot = container.getSlot(pair._1)
       slot.decrStackSize(pair._2)
     })
+
+    // Get the recipe output itemstack
+    // Todo: This will not work on special recipes (Repairing, Cloning Books, Fireworks, etc...)
     val recipeOutput = recipe.getRecipeOutput.copy()
-    val leftOver = container.craftResult.addItem(recipeOutput)
+    val size = container.craftResult.getSizeInventory
+    val leftOver = container.craftResult.condensedAdd(recipeOutput)
     if (!leftOver.isEmpty) {
       container.PlayerInv.player.dropItem(recipeOutput, false)
     }
--- src/main/scala/pfister/quickercrafting/common/util/ExtensionClasses.scala
+++ src/main/scala/pfister/quickercrafting/common/util/ExtensionClasses.scala
@@ -1,6 +1,7 @@
 package pfister.quickercrafting.common.util
 
 import net.minecraft.client.util.RecipeItemHelper
+import net.minecraft.inventory.InventoryBasic
 import net.minecraft.item.ItemStack
 import net.minecraft.item.crafting.IRecipe
 import net.minecraftforge.fml.common.registry.ForgeRegistries
@@ -15,6 +16,46 @@ object ExtensionClasses {
 
   implicit class ExtRecipeItemHelper(recipeItemHelper: RecipeItemHelper) {
     def craftableRecipesIterator(): Iterator[IRecipe] = ForgeRegistries.RECIPES.filterNot(_.isDynamic).filter(recipe => recipeItemHelper.canCraft(recipe, null)).iterator
+  }
+
+  implicit class ExtInventoryBasic(invBasic: InventoryBasic) {
+    // Tries to add an itemstack to an inventory by trying to stack it first, and if that fails put it in the first empty slot
+    def condensedAdd(itemstackToAdd: ItemStack): ItemStack = {
+      val copied = itemstackToAdd.copy()
+      val size = invBasic.getSizeInventory
+      var emptySlotIndex = -1
+      var condensedSlotIndex = -1
+      (0 until size).foreach(i => {
+        val stack = invBasic.getStackInSlot(i)
+        if (stack.isEmpty && emptySlotIndex == -1)
+          emptySlotIndex = i
+        else if (ItemStack.areItemsEqual(copied, stack) && condensedSlotIndex == -1)
+          condensedSlotIndex = i
+      })
+      if (condensedSlotIndex != -1) {
+        val stack = invBasic.getStackInSlot(condensedSlotIndex)
+        val j = Math.min(invBasic.getInventoryStackLimit, stack.getMaxStackSize)
+        val k = Math.min(copied.getCount, j - stack.getCount)
+
+        if (k > 0) {
+          stack.grow(k)
+          copied.shrink(k)
+          if (copied.isEmpty) {
+            invBasic.markDirty()
+            return ItemStack.EMPTY
+          }
+        }
+      }
+      else if (emptySlotIndex != -1) {
+        invBasic.setInventorySlotContents(emptySlotIndex, copied)
+        invBasic.markDirty()
+        return ItemStack.EMPTY
+      }
+      if (copied.getCount != itemstackToAdd.getCount)
+        invBasic.markDirty()
+
+      copied
+    }
   }
 
 
